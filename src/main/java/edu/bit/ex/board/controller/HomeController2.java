f8717a94235316d6ff2b6c230e2db062617b58ab
EUN-HEE\으니 <ghey6002@naver.com>
2021-02-26 11:48:54 +0900

    #contorller 이동

--- src/main/java/edu/bit/ex/board/controller/HomeController2.java
+++ src/main/java/edu/bit/ex/board/controller/HomeController2.java
@@ -1,4 +1,4 @@
-package edu.bit.ex;
+package edu.bit.ex.board.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
